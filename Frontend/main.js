9fc154b85249b3e21606b75735c3006ac2941c13
Shivam Kalra <kalrasahab65@gmail.com>
2026-05-16 23:57:14 +0530

    connections

--- Frontend/main.js
+++ Frontend/main.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       
       try {
-        const response = await fetch("http://localhost:5000/api/analyze", {
+        const response = await fetch("https://scamshieldai-l3w3.onrender.com/api/analyze", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ message })
